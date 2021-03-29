4056e06af2d786cb0bf51be384a7885e4ac1ad5e
Josu <56826352+josugoar@users.noreply.github.com>
2021-03-29 15:02:57 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -1,34 +1,30 @@
 import setuptools
-from os import path
 
 with open("README.md", encoding="utf8") as f:
     long_description = f.read()
 
 setuptools.setup(
     name="argdeco-josugoar",
-    version="1.3.1",
+    version="1.4.0",
     description="Unopinionated argparse wrapper",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="josugoar",
     url="https://github.com/josugoar/argdeco",
+    download_url="https://github.com/josugoar/argdeco/archive/v1.4.0.tar.gz",
     py_modules=["argdeco"],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
-        "Natural Language :: Basque",
-        "Natural Language :: English",
-        "Natural Language :: Spanish",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3.8",
-        "Topic :: Documentation",
         "Topic :: Software Development :: Libraries :: Python Modules"
     ],
     package_data={"argdeco": ["LICENSE", "README.md"]},
     license="MIT",
-    keywords="argparse cli library parsing pip python terminal wrapt",
+    keywords="argparse cli library pipenv python wrapt",
     project_urls={"Source": "https://github.com/josugoar/argdeco"},
     install_requires=["wrapt"],
     python_requires=">=3.8"
